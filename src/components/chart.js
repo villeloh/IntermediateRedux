2af4288bf644121bc43ebd233e83b6c0c7bbba7c
villeloh <villealla@gmail.com>
2018-06-02 23:48:21 +0300

    added temperature, humidity and pressure charts

---
+++ src/components/chart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+export default (props) => {
+
+  const average = (data) => {
+
+    return _.round(_.sum(data)/data.length);
+  };
+
+  return (
+    <div>
+      <Sparklines width={180} height={120} data={props.data}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
+};
---
+++ src/containers/weather_list/weather_list.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../../actions/index';
+import Chart from '../../components/chart';
+
+class WeatherList extends Component {
+
+  renderWeather = (cityData) => {
+
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={name}>
+        <td>
+          {name}
+        </td>
+        <td><Chart data={temps} color={"orange"} units={"K"}/></td>
+        <td><Chart data={pressures} color={"blue"} units={"hPa"}/></td>
+        <td><Chart data={humidities} color={"green"} units={"%"}/></td>
+      </tr>
+    );
+  };
+
+  render() {
+
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+} // WeatherList
+
+
+const mapStateToProps = (state) => {
+
+  return { weather: state.weather };
+};
+
+/* OR: alternate ES-6 syntax:
+const mapStateToProps = ({ weather }) => {
+
+  return { weather }; // ==== return { weather: weather }
+};
+*/
+
+export default connect(mapStateToProps)(WeatherList);
